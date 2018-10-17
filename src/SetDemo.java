267e68cb89fcfd7b1c5f4b6983decf7a42083eb5
gitVictor <victormarzac@yahoo.com>
2018-10-17 10:26:24 +0300

    Initial commit

---
+++ src/SetDemo.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        int count[] ={34,22,10,60,30,22};
+        Set<Integer>  set = new HashSet<>();
+        try {
+
+            for(int i =0; i<5; i++){
+            set.add(count[i]);
+        }
+        System.out.println(set);
+
+        TreeSet sortedSet = new TreeSet<>(set);
+        System.out.println("The sorted list is :");
+        System.out.println(sortedSet);
+
+        System.out.println("The first element of the set is : " + sortedSet.first());
+        System.out.println("The last element in the set is : " + sortedSet.last());
+    }
+    catch (Exception e){
+        }
+    }
+
+}
